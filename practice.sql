0d0a9e05c9d6b763736c920871b550cbee35fd8f
Komalbaghel08 <baghelkomal512@gmail.com>
2025-10-30 21:12:48 +0530

    update

---
+++ practice.sql
@@ -0,0 +1,5 @@
+create database xyz_company;
+use xyz_company;
+create table employee (id int,name varchar(100),salary int);
+insert into employee (id,name,salary) values (1,"komal",2200),(2,'ayush',40000);
+select * from employee;
